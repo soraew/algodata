68f5ae9ada75f6ba7405126771ad3889178f0d84
soraew <soraew@gmail.com>
2021-05-25 12:31:32 +0900

    minor

--- chap5/subset-sumy.c
+++ chap5/subset-sumy.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
+#include "helpers.c"
 #define N 100
 #define B 10000
 
-
-void print_arr_arr(int A[N][B], int size0, int size1){
-   int i, j;
-   for ( i = 0; i < size0; i++)
-   {
-      for ( j = 0; j < size1; j++)
-      {
-         // enum yn temp = 
-         printf("%d  ", A[i][j]);
-      }
-      printf("\n");
-   }
-return;
-}
 
 enum yn {yes, no};
 int main (int argc, char *argv[]) {
@@ -30,4 +17,4 @@ int main (int argc, char *argv[]) {
    print_arr_arr(y, n, b+1);
    printf("first :>> %d\n", first);
    return 0;
-}
+} 
