9a327e109bf37e5645a7fe1d4b3e2ed6944338f4
Sora Ward <soraward@s-nice99.icts.nagoya-u.ac.jp>
2021-05-31 16:06:01 +0900

    problems

---
+++ chap1/problems/2_2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define N 100
+
+struct cell
+{
+   int node;
+   struct cell *next;
+};
+
+void preorder(int k, struct cell **S);
+
+int main (int argc, char *argv[]) {
+   // struct cell *S[N];
+   int root;
+
+   struct cell *S[N] = {1, 3, 6, NULL, NULL, NULL, 8, NULL, NULL, NULL};
+   root = 0;
+   // printf("*S :>> %p\n", S);
+
+   printf("preorder = ");
+   preorder(root, S);
+   printf("\n");
+   return 0;
+}
+
+void preorder(int k, struct cell **S)
+{
+   struct cell *q;
+
+   printf("k :>> %d\n", k);
+   q = S[k];
+   while(q!=NULL){
+      preorder(q->node, S);
+      q = q -> next;
+   }
+   return;
+}
