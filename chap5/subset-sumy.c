790428f03b8a435b97493cc9c4dc5c2396d0f139
Sora Ward <soraward@s-nice99.icts.nagoya-u.ac.jp>
2021-05-31 16:06:55 +0900

    minor changes

--- chap5/subset-sumy.c
+++ chap5/subset-sumy.c
@@ -3,31 +3,26 @@
 #define B 10000
 
 
-void print_arr_arr(int A[N][B], int size0, int size1){
+void print_arr_arr(int y[N][B], int size0, int size1){
    int i, j;
    for ( i = 0; i < size0; i++)
    {
       for ( j = 0; j < size1; j++)
       {
-         // enum yn temp = 
-         printf("%d  ", A[i][j]);
+         printf("%d  ", y[i][j]);
       }
       printf("\n");
    }
 return;
 }
 
-enum yn {yes, no};
+int ssum(int *a, int b, int n){
+   int y[N][B];
+}
+
 int main (int argc, char *argv[]) {
-   int y[N][B], b=11;
-   int k, p=5, n;
-   enum yn first = no;
-   for (k=0; k<n; k++){
-   for (p=0; p<=b; p++){
-      y[k][p] = 0;
-   }
-   }
-   print_arr_arr(y, n, b+1);
+   int n = 5, b = 11, *a={3, 7, 5, 8, 2};
+   // print_arr_arr(y, n, b+1);
    printf("first :>> %d\n", first);
    return 0;
 }
